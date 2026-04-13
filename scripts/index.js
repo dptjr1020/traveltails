22b39e0d412f8fe30be6df23f5c0ea1a0e5e840f
dptjr1020 <dptjr1020@gmail.com>
2026-04-13 15:45:52 +0900

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+const pick = document.querySelector('.spring2026 .pick')
+console.log(pick)
+
+const pickSwiper = new Swiper(pick,{
+    slidesPerView:5,
+    spaceBetween:20,
+    centeredSlides:true,
+})
