00be3832e1d043071afe877a6afe2d3617b01c6f
dptjr1020 <dptjr1020@gmail.com>
2026-04-14 17:13:49 +0900

    코딩

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,8 @@ console.log(pick)
 
 const pickSwiper = new Swiper(pick,{
     slidesPerView:5,
-    spaceBetween:40,
+    spaceBetween:20,
     centeredSlides:true,
     initialSlide:2,
+    loop:true,
 })
