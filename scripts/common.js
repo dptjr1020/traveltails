2cfe6b6e0608c1d9a516605703d87c69107942ce
unknown <dptjr1020@gmail.com>
2026-04-29 22:24:10 +0900

    집에서

--- scripts/common.js
+++ scripts/common.js
@@ -1,7 +1,24 @@
 const headBg = document.querySelector('#head_contents');
-const lnb = document.querySelectorAll('#gnb .lnb')
-console.log(headBg, headerBtn, lnb);
 
-const lnbList = document.querySelectorAll('#gnb .lnb');
+const gnb = document.querySelectorAll('#gnb > li');
 
+for (let i of gnb) {
+    const link = i.children[0]; // a 태그
+    const lnb = i.children[1];  // .lnb
 
+    if (!lnb) continue;
+
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        // 전체 닫기
+        for (let j of gnb) {
+            if (j.children[1]) {
+                j.children[1].style.display = 'none';
+            }
+        }
+
+        // 클릭한 것만 열기
+        lnb.style.display = 'flex';
+    });
+}
